4a903bfa5a8bfa18945ff98ecb36aa5c9c3f0f62
Matt McElwee <mubatt@wyopub.com>
2017-11-05 11:25:52 -0600

    Added meta data for series pages

--- src/app/series/series.component.ts
+++ src/app/series/series.component.ts
@@ -15,7 +15,7 @@ export class SeriesComponent implements OnInit {
 	sub: any;
 	permalink: string;
 
-  constructor(private http: Http, private route: ActivatedRoute) { }
+  constructor(private http: Http, private route: ActivatedRoute, private title: Title, private meta: Meta) { }
 
 	ngOnInit() {
 		this.sub = this.route.params.subscribe(params => {
@@ -23,6 +23,18 @@ export class SeriesComponent implements OnInit {
 			this.http.request(`https://api.flatlandchurch.com/v2/series/${params['permalink']}?key=pk_e6afff4e5ad186e9ce389cc21c225`)
 				.subscribe((res: Response) => {
 					this.series = res.json();
+					this.title.setTitle(this.series['title'] + ' | Flatland Church');
+					this.meta.addTags([
+						{ name: 'description', content: this.series['description'] },
+						{ property: 'og:url', content: `https://flatlandchurch.com/series/${this.series['permalink']}` },
+						{ property: 'og:image', content: this.series['image'] },
+						{ name: 'twitter:title', content: this.series['title'] },
+						{ name: 'twitter:description', content: this.series['description'] },
+						{ name: 'twitter:image', content: this.series['image'] },
+						{ property: 'og:title', content: this.series['title'] },
+						{ property: 'place:location:latitude', content: '41.3039152' },
+						{ property: 'place:location:longitude', content: '-96.1377482' }
+					]);
 				});
 		});
   }
