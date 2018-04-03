936b360434bea6cea9fda8bb7db24bd3452d0e49
Matt McElwee <mubatt@wyopub.com>
2018-04-02 22:15:43 -0500

    Added 404 handling to videos and series

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-not-found',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(private meta: Meta, private title: Title) { }
 
   ngOnInit() {
+		this.title.setTitle('Page Not Found | Flatland Church');
+		this.meta.addTags([
+			{ name: 'description', content: 'Flatland Church is all about helping you move closer to the center of God\'s kingdom where real life transformation happens.' },
+			{ property: 'og:url', content: `https://flatlandchurch.com/404` },
+			{ property: 'og:image', content: '' },
+			{ name: 'twitter:title', content: 'Page Not Found' },
+			{ name: 'twitter:description', content: 'Flatland Church is all about helping you move closer to the center of God\'s kingdom where real life transformation happens.' },
+			{ name: 'twitter:image', content: '' },
+			{ property: 'og:title', content: 'Page Not Found' },
+			{ property: 'place:location:latitude', content: '41.3039152' },
+			{ property: 'place:location:longitude', content: '-96.1377482' }
+		]);
   }
 
 }
