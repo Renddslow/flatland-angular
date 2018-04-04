8ce12d0b9980aed33a06ee1548fbf7f4de88447c
Matt McElwee <mmcelwee@dmsi.com>
2018-04-04 14:19:35 -0500

    Made class page improvements - Sort classes by order params - Add labels of type to classes - Created a featured params to make a double-wide class card

--- src/app/page/page-image-card/page-image-card.component.ts
+++ src/app/page/page-image-card/page-image-card.component.ts
@@ -10,107 +10,116 @@ import { startCase } from 'lodash';
 })
 export class PageImageCardComponent implements OnInit {
 
-	@Input() type: string;
-	@Input() permalink?: string;
-	@Input() topic?: string;
+  @Input() type: string;
+  @Input() permalink?: string;
+  @Input() topic?: string;
 
-	sermons = [];
-	events = [];
-	posts = [];
-	classes = [];
-	groups = [];
+  sermons = [];
+  events = [];
+  posts = [];
+  classes = [];
+  groups = [];
 
-	page: number = 0;
-	offset: number = 0;
+  page: number = 0;
+  offset: number = 0;
 
   constructor(private http: Http) {}
 
   ngOnInit() {
-		if (this.type.includes("watch")) {
-			this.getSermons();
-		} else if (this.type.includes("recommended")) {
-			this.getRecommendations(this.permalink);
-		} else if (this.type.includes("enjoy")) {
-			this.http.request(`https://api.flatlandchurch.com/v2/events?key=pk_e6afff4e5ad186e9ce389cc21c225`)
-				.subscribe((res: Response) => {
-					const events = res.json().map(event => {
-						event.date = moment(event.startsAt * 1000).format('MMMM D');
-						return event;
-					});
-					this.events = events;
-				});
-		} else if (this.type.includes('blog')) {
-			this.getBlogPosts(this.topic);
-		} else if (this.type.includes('classes')) {
-			this.getClasses();
-		} else if (this.type.includes('groups')) {
-			this.getGroups();
-		}
+    if (this.type.includes('watch')) {
+      this.getSermons();
+    } else if (this.type.includes('recommended')) {
+      this.getRecommendations(this.permalink);
+    } else if (this.type.includes('enjoy')) {
+      this.http.request(`https://api.flatlandchurch.com/v2/events?key=pk_e6afff4e5ad186e9ce389cc21c225`)
+        .subscribe((res: Response) => {
+          const events = res.json().map(event => {
+            event.date = moment(event.startsAt * 1000).format('MMMM D');
+            return event;
+          });
+          this.events = events;
+        });
+    } else if (this.type.includes('blog')) {
+      this.getBlogPosts(this.topic);
+    } else if (this.type.includes('classes')) {
+      this.getClasses();
+    } else if (this.type.includes('groups')) {
+      this.getGroups();
+    }
   }
 
-	loadMore = (event, type) => {
-		event.preventDefault();
-		switch(type) {
-			case "blog":
-				this.getBlogPosts(this.topic);
-				if (this.topic) {
-					history.pushState(null, null, `/blog/topic/${this.topic}?page=${this.page + 1}`);
-				} else {
-					history.pushState(null, null, `/blog?page=${this.page + 1}`);
-				}
-				break;
-			case "watch":
-				this.getSermons();
-		}
-	}
+  loadMore = (event, type) => {
+    event.preventDefault();
+    switch(type) {
+      case "blog":
+        this.getBlogPosts(this.topic);
+        if (this.topic) {
+          history.pushState(null, null, `/blog/topic/${this.topic}?page=${this.page + 1}`);
+        } else {
+          history.pushState(null, null, `/blog?page=${this.page + 1}`);
+        }
+        break;
+      case "watch":
+        this.getSermons();
+    }
+  }
 
-	getBlogPosts = (topic?) => {
-		const baseURL = `https://api.flatlandchurch.com/v2/blog?key=pk_e6afff4e5ad186e9ce389cc21c225`;
-		const url = topic
-			? `${baseURL}&filter[topic]=${topic}&page[offset]=${this.offset}`
-			: `${baseURL}&page[offset]=${this.offset}`;
-		this.http.request(url)
-			.subscribe((res: Response) => {
-				this.posts = this.posts.concat(res.json().map(p => ({
-					...p,
-					date: moment.unix(p.date).format('MMM D, YYYY'),
-					tags: Object.keys(p.topics)
-						.map(t => startCase(t))
-						.filter(t => t !== 'Legacy')
-				})));
-				this.page += 1;
-				this.offset = this.page * 12;
-			});
-	};
+  getBlogPosts = (topic?) => {
+    const baseURL = `https://api.flatlandchurch.com/v2/blog?key=pk_e6afff4e5ad186e9ce389cc21c225`;
+    const url = topic
+      ? `${baseURL}&filter[topic]=${topic}&page[offset]=${this.offset}`
+      : `${baseURL}&page[offset]=${this.offset}`;
+    this.http.request(url)
+      .subscribe((res: Response) => {
+        this.posts = this.posts.concat(res.json().map(p => ({
+          ...p,
+          date: moment.unix(p.date).format('MMM D, YYYY'),
+          tags: Object.keys(p.topics)
+            .map(t => startCase(t))
+            .filter(t => t !== 'Legacy')
+        })));
+        this.page += 1;
+        this.offset = this.page * 12;
+      });
+  };
 
-	getSermons = () => {
-		this.http.request(`https://api.flatlandchurch.com/v2/sermons?page=${this.page}&key=pk_e6afff4e5ad186e9ce389cc21c225`)
-			.subscribe((res: Response) => {
-				this.sermons = this.sermons.concat(res.json());
-				this.page += 1;
-			});
-	};
+  getSermons = () => {
+    this.http.request(`https://api.flatlandchurch.com/v2/sermons?page=${this.page}&key=pk_e6afff4e5ad186e9ce389cc21c225`)
+      .subscribe((res: Response) => {
+        this.sermons = this.sermons.concat(res.json());
+        this.page += 1;
+      });
+  };
 
-	getClasses = () => {
-		this.http.request('https://api.flatlandchurch.com/v2/classes?key=pk_e6afff4e5ad186e9ce389cc21c225')
-			.subscribe((res: Response) => {
-				this.classes = this.classes.concat(res.json());
-			});
-	}
+  getClasses = () => {
+    this.http.request('https://api.flatlandchurch.com/v2/classes?key=pk_e6afff4e5ad186e9ce389cc21c225')
+      .subscribe((res: Response) => {
+        this.classes = this.classes.concat(res.json());
+        this.classes.sort((a, b) => {
+          if (a.order > b.order) {
+            return 1;
+          }
+          if (a.order < b.order) {
+            return -1;
+          }
+          return 0;
+        });
+      });
+  }
 
-	getRecommendations = (permalink) => {
-		this.http.request(`https://api.flatlandchurch.com/v2/sermons/${permalink}/series?key=pk_e6afff4e5ad186e9ce389cc21c225`)
-			.subscribe((res: Response) => {
-				this.sermons = res.json().slice(0, 3);
-			});
-	}
+  getRecommendations = (permalink) => {
+    this.http.request(`https://api.flatlandchurch.com/v2/sermons/${permalink}/series?key=pk_e6afff4e5ad186e9ce389cc21c225`)
+      .subscribe((res: Response) => {
+        this.sermons = res.json().slice(0, 3);
+      });
+  }
 
-	getGroups = () => {
-		this.http.request(`https://api.flatlandchurch.com/v2/groups?key=pk_e6afff4e5ad186e9ce389cc21c225`)
-			.subscribe((res: Response) => {
-				this.groups = res.json();
-				console.log(this.groups)
-			});
-	}
+  getGroups = () => {
+    this.http.request(`https://api.flatlandchurch.com/v2/groups?key=pk_e6afff4e5ad186e9ce389cc21c225`)
+      .subscribe((res: Response) => {
+        this.groups = res.json();
+        console.log(this.groups)
+      });
+  }
 
 }
